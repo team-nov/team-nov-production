55abe3de95dd4eebefd94c4f53733f0f98007fe5
Calvin <calvin.yuen@mail.utoronto.ca>
2021-06-08 23:22:47 -0400

    Add User component

---
+++ frontend/src/User/User.jsx
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+import axios from 'axios'
+import './User.css'
+
+class User extends Component{
+  state = {
+    username: "calvin yuen",
+    picture: "https://picsum.photos/500"
+  }
+
+  render(){
+    return (
+      <div className="user">
+        <img className="profilePic" src={this.state.picture} alt=""/>
+        <span className="userName"> {this.state.username} </span>
+      </div>
+    )
+  }
+}
+export default User;
