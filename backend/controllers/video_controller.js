0c94cffabf445a36bf7c3df3f7df7210dc8ce6a7
Brandon Lo <brandonholun.lo@gmail.com>
2021-06-09 21:33:00 -0400

    [TNOV-52] Add GET for specific video ID and POST for video comments

--- backend/controllers/video_controller.js
+++ backend/controllers/video_controller.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Video = require('../models/video_model');
+const Comment = require('../models/comment_model');
 
 exports.getVideos = (req,res,next) => {
     Video
         .find()   
+        .exec()
+        .then((data)=>{
+            res.status(200).json(data)
+        })
+}
+
+exports.getVideoById = (req,res,next) => {
+    const videoId = req.params.videoId
+    Video
+        .findOne({_id:videoId})
         .exec()
         .then((data)=>{
             res.status(200).json(data)
@@ -25,6 +36,47 @@ exports.postVideos = (req,res,next) => {
                 message_return:"Successfully added video",
                 video: video
             })
+        })
+        .catch( (err) => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            })
+        });
+}
+
+exports.postCommentsByVideoId = (req,res,next) => {
+    const comment = new Comment({
+        _id: new mongoose.Types.ObjectId(),
+        userName: req.body.userName,
+        userPicture: req.body.userPicture,
+        message: req.body.message,
+        userId: req.body.userId,
+        postTime: new Date(),
+    })
+
+    const videoId = req.params.videoId;
+
+    Video
+        .findOne({_id:videoId})
+        .exec()
+        .then((video) => {
+            video.comments.push(comment)
+            video
+                .save()
+                .then( (result) => {
+                    console.log(result)
+                    res.status(201).json({
+                        message_return:"Successfully added comment to video",
+                        video: video
+                    })
+                })
+                .catch( (err) => {
+                    console.log(err);
+                    res.status(500).json({
+                        error:err
+                    })
+                });
         })
         .catch( (err) => {
             console.log(err);
--- backend/routes/video_routes.js
+++ backend/routes/video_routes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const videosController = require('../controllers/video_controller');
 
 router.get('/', videosController.getVideos);
+router.get('/:videoId', videosController.getVideoById);
 router.post('/', videosController.postVideos);
+router.post('/:videoId', videosController.postCommentsByVideoId);
 
 module.exports = router
