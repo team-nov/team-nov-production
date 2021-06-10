bb161864aec621a69781614095bef5f10c4955e6
Brandon Lo <brandonholun.lo@gmail.com>
2021-06-10 00:50:35 -0400

    [TNOV-52] Minor changes to comment schema

--- backend/models/comment_model.js
+++ backend/models/comment_model.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    //commentId: {type: String, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, required:true},
     userName:{type:String, required:true},
-    // userPicture:{type:String, default: "s"},
-    // userId: {type: String, default:"nullID"},
     message:{type:String, required:true},
     postTime: {type: String, required: true},
 });
