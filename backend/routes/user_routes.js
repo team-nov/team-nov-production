347aecab42a7f8facf3a08ef263f370e378403f0
Brandon Lo <brandonholun.lo@gmail.com>
2021-06-11 17:28:30 -0400

    [TNOV-56] Add navigation bar for accessing the chat feed

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const usersController = require('../controllers/user_controller');
 
 // Added user schema for registration
+router.get('/:userId',usersController.getUserById);
 router.get('/',usersController.getUsers);
 router.post('/',usersController.postUsers);
 
