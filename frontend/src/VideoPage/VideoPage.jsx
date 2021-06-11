a9c013dd2a50d53dcde5931299be2b4e0b4d7438
Brandon Lo <brandonholun.lo@gmail.com>
2021-06-10 21:15:52 -0400

    [TNOV-54] Update comments section in video page allowing users to comment

--- frontend/src/VideoPage/VideoPage.jsx
+++ frontend/src/VideoPage/VideoPage.jsx
@@ -1,19 +1,57 @@
 import { Link } from "react-router-dom";
+import axios from 'axios'
 import React, {Component} from 'react';
 
 class VideoPage extends Component {
 
     state = {
+        msg: '',
         comments: []
     }
 
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/videos/60c2affa6d8ff3030fd4ea01').then(
+            (res)=>{
+                console.log(res.data.comments);
+                this.setState({
+                    comments: res.data.comments
+                })
+            }
+        )
+    }
+
+    postComment = () => {
+        axios.post('http://localhost:5000/api/videos/60c2affa6d8ff3030fd4ea01', {
+            userId: "60c27574bf5cbfadcf3b4f12",
+            userName: "Brandon",
+            message: this.state.msg
+        })
+        .then(res => alert(res.data.message))
+    }
+
+    updateComment = (e) => {
+        this.setState({msg: e.target.value})
+    }
+
     render(){
+        let commentsSection = this.state.comments.map((comment)=>{
+            return <li>
+                {comment.userName}: {comment.message}
+            </li>
+        })
         return(
-            <div class="container">
+            <div className="container">
                 <h1>Video Title Here</h1>
-                <div class="container">
-                    <textarea type="text" className="form-control" placeholder="Comment here"/>
+                <h2>Comments Section:</h2>
+                <div className="container">
+                    <ul>
+                        {commentsSection}
+                    </ul>
                 </div>
+                <div className="container">
+                    <textarea onChange={(e) => this.updateComment(e)} type="text" className="form-control" placeholder="Comment here"/>
+                </div>
+                <button onClick={this.postComment}>Post Comment</button>
                 <Link className="nav-link" to="/videos">Back</Link>
             </div>
         )
