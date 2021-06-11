6666f7edaf1778efeb97b97a9e0a4d7a7faa9ac4
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2021-06-11 00:41:37 -0400

    [TNOV-33] Renamed controller file

--- backend/routes/users_routes.js
+++ backend/routes/users_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const usersController = require('../controllers/user_controllers');
+const usersController = require('../controllers/user_controller');
 
 router.get('/',usersController.getUsers);
 router.post('/',usersController.postUsers);
