cb60ac3cc5892ef98f8937366708a23536acda2d
Jahin <jahinpromit@gmail.com>
2021-07-01 02:38:20 -0400

    [TNOV-67] Added a discard and submit button for editing posts.

--- frontend/src/Discussion/Discussion.jsx
+++ frontend/src/Discussion/Discussion.jsx
@@ -4,13 +4,41 @@ import User from '../User/User'
 
 class Discussion extends Component{
   state = {
-    message: this.props.message,
+    isHidden: false,
+    initialMessage: this.props.message,
+    currentMessage: this.props.message,
   }
 
+  onEditClick=()=>{
+    this.setState({isHidden:!this.state.isHidden})
+  }
+
+  onDiscardClick=()=>{
+    this.setState({currentMessage: this.state.initialMessage})
+    this.setState({isHidden:!this.state.isHidden})
+  }
+
+  onSubmitClick=()=>{
+    this.setState({initialMessage: this.state.currentMessage})
+    this.setState({isHidden:!this.state.isHidden})
+  }
+  
+  onClick=()=>{
+    this.setState({isHidden:!this.state.isHidden})
+  }
+
+  edit=(e)=>{
+    this.setState({currentMessage:e.target.value})
+  }
 
   modifyText=(e)=> {
+    console.log( e.target.parentNode.style);
+    //e.target.parentNode.style.display = 'none';
+    e.target.style.display = 'none';
+
     this.setState({
-      message: 'Changed message'
+      message: 'Changed message',
+      isHidden: !this.state.isHidden
     });
   }
 
@@ -20,10 +48,13 @@ class Discussion extends Component{
       <div className="discussionContainer">
         <User username={this.props.username} picture={this.props.picture}/>
         
-        <div className="DiscussionMessage"> {this.state.message} </div>
+        <div className="DiscussionMessage" style={{display: this.state.isHidden?"none":"block"}}> {this.state.currentMessage} </div>
+        <textarea  className="postTextEntry" onChange={(e)=>{this.edit(e)}} style={{display: !this.state.isHidden?"none":"block"}} value={this.state.currentMessage} rows="4" cols="100" placeholder="Start a Discussion..."></textarea>
         <div className="postTime">{this.props.postTime}</div>
         <div className="modifyPost">
-          {this.props.ownDiscussion ? (<button className="modifyButton" onClick={this.modifyText}>Edit</button>) : (<></>)}
+          {this.props.ownDiscussion ? (<button className="editButton" onClick={this.onEditClick} style={{display: this.state.isHidden?"none":"block"}}>Edit</button>) : (<></>)}
+          {this.props.ownDiscussion ? (<button className="discardButton" onClick={this.onDiscardClick} style={{display: !this.state.isHidden?"none":"block"}}>Discard</button>) : (<></>)}
+          {this.props.ownDiscussion ? (<button className="submitButton" onClick={this.onSubmitClick} style={{display: !this.state.isHidden?"none":"block"}}>Submit</button>) : (<></>)}
         </div>
         
         
