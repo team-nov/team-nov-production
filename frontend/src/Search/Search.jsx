e33131c289fe0826550ebb7db29abcad50e44923
Colin Lin <colincool100@gmail.com>
2021-07-01 18:34:19 -0400

    [TNOV-62] Added search bar styling and error checking
    Added styling to search bar
    added check for empty queries

--- frontend/src/Search/Search.jsx
+++ frontend/src/Search/Search.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import searchTheme from './Search.module.css'
+import './Search.css'
 const serverUrl = "http://localhost:5000/api";
 class Search extends Component{
     state={
@@ -15,75 +15,83 @@ class Search extends Component{
     getAllResults = async()=>{
         let res = await axios.get(serverUrl+"/videos");
         await this.setState({results:res.data});
-        this.resultsToSuggestions()
+        this.resultsToSuggestions(res.data)
     }
-    getResultsByQuery =async()=>{
+    getSearchSuggestions = async()=>{
         let res = await axios.get(serverUrl+"/videos/search/"+this.state.value);
-        await this.setState({results:res.data});
-        this.resultsToSuggestions()
-
+        this.resultsToSuggestions(res.data)
     }
-    resultsToSuggestions = async()=>{
-        let suggestions = this.state.results.map(res=>res.title)
+    getSearchResults =async()=>{
+        let res = await axios.get(serverUrl+"/videos/search/"+this.state.value);
+        this.setState({results:res.data, suggestions:[]})
+    }
+    resultsToSuggestions = async(results)=>{
+        let suggestions = results.map(res=>res.title)
         this.setState({suggestions:suggestions});
        
     }
-    onChange = async (e)=>{
+    onSearchInputChange = async (e)=>{
        let value =  e.target.value;
        await this.setState({value:value});
-       this.getResultsByQuery();
+       this.getSearchSuggestions();
     }
-    onClick =async(e)=>{
+    onSuggestionClick =async(e)=>{
         let value =  e.target.innerText;
         console.log(value)
         await this.setState({value:value});
-        this.getResultsByQuery();
+        this.getSearchResults();
+    }
+    onSearchButtonClick =async()=>{
+        if(!this.state.value){
+            alert("Please enter a search query")
+        }
+        else
+            this.getSearchResults();
     }
     render(){
         let options = this.state.value 
             ? this.state.suggestions.map((suggest,index)=>{
-                return <li class="list-group-item" key={index} onClick={(e)=>this.onClick(e)}>{suggest}</li>
+                return <li class="list-group-item" style={{textAlign:'left'}}key={index} onClick={(e)=>this.onSuggestionClick(e)}>{suggest}</li>
                 })
             : []
         let videos = this.state.results.map((video,index)=>{
             return (
-            <div class="card col-3 m-3" style={{"width": "18rem"}}>
+            <div class="card col-2 m-3" >
                 <img class="card-img-top" src="https://via.placeholder.com/150" alt="oops"/>
                 <div class="card-body">
                     <h5 class="card-title">{video.title}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-primary">Watch video</a>
                 </div>
             </div>
           )
         })
-        let groupSize=4;
-        let videosGrouped=[];
-        for(let i=0;i<videos.length/groupSize;i++){
-            if(i+groupSize-1<videos.length){
-                videosGrouped[i] = <div class="row ">
-                    {videos.slice(i,i+groupSize)}
-                </div>
-            }
-            else{
-                videosGrouped[i] = <div class="row ">
-                    {videos.slice(i,videos.length)}
-                </div>
 
-            }
-
-        }
+        if(videos.length ===0)
+            videos=<h1>No Results found</h1>
+        
         return(
 
             <div>
-                <div className="search">
-                    <input type="search" value={this.state.value} onChange={(e)=>this.onChange(e)} />
-                    <ul class="list-group">
-                        {options}
-                    </ul>
+                <div className="row justify-content-center">
+                    <div className="input-group w-75">
+                        <div className="w-75">
+                            <input className="form-control" type="search" value={this.state.value} onChange={(e)=>this.onSearchInputChange(e)} />
+                        </div>
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" onClick={this.onSearchButtonClick}>Search</button>
+                        </div>
+                        <ul class="list-group w-75 ">
+                            {options}
+                        </ul>
+                    </div>
+                    
                 </div>
+                
                 <div className="container-fluid">
-                   {videosGrouped}
+                <div className="d-flex flex-row flex-wrap justify-content-around p-5">
+                    {videos}
+                </div>
 
                 </div>
             </div>
