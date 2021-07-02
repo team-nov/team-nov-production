0c5697f48cbfda6e071f0eb3ad470029bf463058
SimonYChau <83619071+SimonYChau@users.noreply.github.com>
2021-07-02 13:38:43 -0400

    [TNOV-66] Deleted commented out code

--- frontend/src/Video/Video.jsx
+++ frontend/src/Video/Video.jsx
@@ -1,26 +1,17 @@
 import './Video.css'
 
 const Video = props => (
-  // <div class="card col-3 m-4" style={{"width": "18rem"}}>
-  // <img class="card-img-top" src="https://picsum.photos/200/300" alt="oops"/>
-  // <div class="card-body">
-  //     <h5 class="card-title">{props.title}</h5>
-  //     <p class="card-text">The description of the video. The description of the video. The description of the video.</p>
-  //     <a href={"/videos/" + props.id} class="btn btn-primary">Watch video</a>
-  // </div>
-  // </div>
-
-<div class="card col-3 m-4" style={{"width": "18rem"}}>
-<img class="card-img-top" src="https://picsum.photos/300/200" alt="off"/>
-  <div class="card-body">
-  <h5 class="card-title">{props.title}</h5>
-  <h7 class="video-author">
-    <img src="https://picsum.photos/100/100" alt="oof" className="profile-pic"></img>
-    *Author name
-  </h7>
-  <a href={"/videos/" + props.id} class="btn btn-primary stretched-link">Watch video</a>
+  <div class="card col-3 m-4" style={{"width": "18rem"}}>
+  <img class="card-img-top" src="https://picsum.photos/300/200" alt="off"/>
+    <div class="card-body">
+    <h5 class="card-title">{props.title}</h5>
+    <h7 class="video-author">
+      <img src="https://picsum.photos/100/100" alt="oof" className="profile-pic"></img>
+      *Author name
+    </h7>
+    <a href={"/videos/" + props.id} class="btn btn-primary stretched-link">Watch video</a>
+    </div>
   </div>
-</div>
 )
 
 export default Video;
