d8e7f33ab37368bd5712aa0c13cb82e72198d0bc
Calvin Yuen <calvin.yuen@mail.utoronto.ca>
2021-07-02 01:08:39 -0400

    [TNOV-72] Rearrange the order of the routes

--- backend/routes/discussion_routes.js
+++ backend/routes/discussion_routes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 const discussionsController = require('../controllers/discussion_controller');
 
-router.get('/', discussionsController.getDiscussions);
 router.get('/:discussionId', discussionsController.getOneDiscussion);
-router.post('/', discussionsController.postDiscussions);
-router.patch('/', discussionsController.patchDiscussion);
-router.delete('/', discussionsController.deleteDiscussion);
 router.post('/:discussionId', discussionsController.postComment);
 router.delete('/:discussionId', discussionsController.deleteComment);
 router.patch('/:discussionId', discussionsController.patchComment);
+router.get('/', discussionsController.getDiscussions);
+router.post('/', discussionsController.postDiscussions);
+router.patch('/', discussionsController.patchDiscussion);
+router.delete('/', discussionsController.deleteDiscussion);
 
 module.exports = router;
