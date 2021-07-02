174be1e2fdcba3286a0cb24d3bc23da7ac77b2ed
SimonYChau <83619071+SimonYChau@users.noreply.github.com>
2021-07-02 13:54:41 -0400

    [TNOV-66] Changed font sizes

--- frontend/src/Video/Video.jsx
+++ frontend/src/Video/Video.jsx
@@ -4,11 +4,11 @@ const Video = props => (
   <div class="card col-3 m-4" style={{"width": "18rem"}}>
   <img class="card-img-top" src="https://picsum.photos/300/200" alt="off"/>
     <div class="card-body">
-    <h5 class="card-title">{props.title}</h5>
-    <h7 class="video-author">
+    <h4 class="card-title">{props.title}</h4>
+    <h6 class="video-author">
       <img src="https://picsum.photos/100/100" alt="oof" className="profile-pic"></img>
-      *Author name
-    </h7>
+      *Author name*
+    </h6>
     <a href={"/videos/" + props.id} class="btn btn-primary stretched-link">Watch video</a>
     </div>
   </div>
