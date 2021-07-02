9b2a636a587fa72ce03b1ac10d9dba172734c5b1
Calvin Yuen <calvin.yuen@mail.utoronto.ca>
2021-07-02 00:02:55 -0400

    [TNOV-72] Add routes to discussion_routes for all discussion comment features

--- backend/routes/discussion_routes.js
+++ backend/routes/discussion_routes.js
@@ -10,5 +10,8 @@ router.get('/:discussionId', discussionsController.getOneDiscussion);
 router.post('/', discussionsController.postDiscussions);
 router.patch('/', discussionsController.patchDiscussion);
 router.delete('/', discussionsController.deleteDiscussion);
+router.post('/:discussionId', discussionsController.postComment);
+router.delete('/:discussionId', discussionsController.deleteComment);
+router.patch('/:discussionId', discussionsController.patchComment);
 
 module.exports = router;
