cdbbf2020057da2dcb6294744a1c3c340c84218e
SimonYChau <83619071+SimonYChau@users.noreply.github.com>
2021-07-10 17:43:23 -0400

    [TNOV-19] Added methods and a form to enter information to CompanyPage

--- frontend/src/CompanyPage/CompanyPage.jsx
+++ frontend/src/CompanyPage/CompanyPage.jsx
@@ -4,8 +4,77 @@ import './CompanyPage.css'
 
 class CompanyPage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        // _id:sessionStorage.getItem('_id'),
+        company:'',
+        companyLogo:'',
+        companyLocation:'',
+        companyDescription:''
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+        [event.target.name] : event.target.value
+    })
+  }
+
+  onSubmit(e) {
+
+    e.preventDefault();
+
+    const company = {
+
+      company: this.state.company,
+      companyLogo: this.state.companyLogo,
+      companyLocation: this.state.companyLocation,
+      companyDescription: this.state.companyDescription
+    }
+
+    console.log(company);
+
+    axios.post('http://localhost:5000/company', company)
+      .then(res=> {
+          if(res.data.success) {
+              alert("submitted form");
+          } else {
+              alert("submit failure");
+          }
+      })
+      .catch((error)=> {
+          alert("invalid form");
+      });
+  }
+
   render(){
-      return(<h1>"Hi welcome to the your company page"</h1>)
+    return(
+      <div className="CompanyPage">
+        <form className='CompanyForm' onSubmit={this.onSubmit}>
+            <div className='field'>
+                <label>Company Name: </label>
+                <input type='text' name='company' value={this.state.company} onChange={this.onChange}></input>
+            </div>
+            <div className='field'>
+                <label>Logo: </label>
+                <input type='text' name='companyLogo' value={this.state.companyLogo} onChange={this.onChange}></input>
+            </div>
+            <div className='field'>
+                <label>Location: </label>
+                <input type='text' name='companyLocation' value={this.state.companyLocation} onChange={this.onChange}></input>
+            </div>
+            <div className='field'>
+                <label>Description: </label>
+                <input type='text' name='companyDescription' value={this.state.companyDescription} onChange={this.onChange}></input>
+            </div>
+            <input className='CompanyButton' type='submit' value="Save Changes"></input>
+        </form>
+      </div>
+    )
   }
 }
 
