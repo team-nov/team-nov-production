d856b72e1fbd52f129de6f568e76259180b5e41b
Calvin Yuen <calvin.yuen@mail.utoronto.ca>
2021-07-16 21:08:05 -0400

    [TNOV-58] Fix bug in discussion

--- frontend/src/Forum/Forum.jsx
+++ frontend/src/Forum/Forum.jsx
@@ -4,21 +4,6 @@ import './Forum.css'
 import Discussion from '../Discussion/Discussion'
 import User from '../User/User'
 import { dateParser } from '../utils/DateParser'
-import firebase from "firebase/app";
-import "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAazLY-icEO67sl2FHkiA1MD97mhMGNCqQ",
-  authDomain: "project-team-nov.firebaseapp.com",
-  projectId: "project-team-nov",
-  storageBucket: "project-team-nov.appspot.com",
-  messagingSenderId: "866523261997",
-  appId: "1:866523261997:web:1ece7eedc92753e287dca4",
-  measurementId: "G-JX5S7P355F"
-};
-
-firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
 
 class Forum extends Component{
   state = {
@@ -26,8 +11,6 @@ class Forum extends Component{
     picture: '',
     name: sessionStorage.getItem("name"),
     message: '',
-    image: {},
-    imageURL: '',
     discussions: [],
   }
 
@@ -42,45 +25,14 @@ class Forum extends Component{
       .catch((e)=>console.log(e))
   }
 
-  handleChange=(e)=>{
-    if (e.target.files[0]) { 
-        const image = e.target.files[0];
-        console.log(image);
-        this.setState(() => ({ image: image }));
-    }
-  }
-
   updateInput=(e)=>{
     this.setState({message:e.target.value});
   }
 
   addDiscussion=()=>{
-    if (this.state.image !== null) {
-      if (this.state.image.type.substr(0, this.state.image.type.indexOf("/")) !== "image") {
-        alert("Please submit an image type file.");
-        return;
-      }
-      const uploadTask = storage.ref(`images/${this.state.image.name}`).put(this.state.image);
-      uploadTask.on(
-        "state_changed",
-        snapshot => {},
-        error => {alert(error)},
-        () => {
-          storage.ref('images')
-                 .child(this.state.image.name)
-                 .getDownloadURL()
-                 .then(url => {
-                   alert(URL);
-                   this.setState({imageURL: url});
-                 });
-        }
-      );
-    }
-
     axios.post('http://localhost:5000/api/discussions', {
       userId: this.state.userId,
       message: this.state.message,
-      imageURL: this.state.imageURL
     })
     .then(res=>this.setState({discussions: res.data}))
     .catch((e)=>console.log(e))
@@ -106,7 +58,6 @@ class Forum extends Component{
         <form className="postInput">
           <textarea onChange={(e)=>this.updateInput(e)} value={this.state.message} className="postTextEntry" rows="4" cols="100" placeholder="Start a Discussion..."></textarea>
           <button className="postButton" onClick={this.addDiscussion}> Post </button>
-          <input type="file" onChange={this.handleChange}/>
         </form>
       </div>
       <div className="discussions">
